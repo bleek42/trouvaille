4ffcee950275ccf0c799a8df2a04b660202fa7ea
bleek42 <brandonleek42@gmail.com>
2020-12-06 18:36:40 -0500

    useCallback added to handleChange with correct typeDefs now working

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,29 +1,49 @@
-import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { useCallback } from 'react';
+import { View, Text, TextInput } from 'react-native';
 
 interface FormState {
-  chars: EventTarget | null;
-  submitted?: TouchEvent;
+  username: string | undefined;
+  email: string | undefined;
+  password: string | undefined;
+  pwMatch?: boolean;
+  submitted?: boolean;
 }
 
 const Register: React.FC = () => {
-  const [chars, setChars] = useState<FormState>({
-    chars: null,
+  const [form, setForm] = useState<FormState>({
+    username: '',
+    email: '',
+    password: '',
+    pwMatch: true,
     submitted: false,
   });
 
-  const handleChange = (ev: React.ChangeEvent<HTMLInputElement>): void => {
-    setChars({
-      chars: ev.target,
-    });
-  };
+  const inputRef = useRef<TextInput>(null);
 
-  const handleSubmit = (ev: React.TouchEvent): void => {
-    ev.preventDefault();
-  };
+  const handleChange = useCallback(
+    (ev: React.ChangeEvent<{ name: any; value: string }>) => {
+      const { name, value } = ev.target;
+      setForm({
+        ...form,
+        [name]: value,
+      });
+    },
+    [form],
+  );
+
+  // const handleSubmit = (ev: React.TouchEvent): void => {
+  //   ev.preventDefault();
+  // };
+
   return (
     <View>
       <Text>register</Text>
+      <TextInput
+        ref={inputRef}
+        value={form.username}
+        onChangeText={() => handleChange}
+      />
     </View>
   );
 };
